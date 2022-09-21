6b54f19eca5119fd92274a4bd3db5c2a76d0824b
Chris Moss <hcmoss70@gmail.com>
2022-09-20 23:13:17 -0400

    added views

--- guitar-inventory/dist/index.js
+++ guitar-inventory/dist/index.js
@@ -4,10 +4,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const path_1 = __importDefault(require("path"));
 const app = (0, express_1.default)();
 const port = 8080;
+// configure express to use EJS
+app.set("views", path_1.default.join(__dirname, "views"));
+app.set("view engine", "ejs");
 app.get('/', (req, res) => {
-    res.send('hello there');
+    // render the ejs template
+    res.render("index");
 });
 app.listen(port, () => {
     // tslint:disable-next-line:no-console
--- guitar-inventory/src/index.ts
+++ guitar-inventory/src/index.ts
@@ -1,10 +1,15 @@
 import express from "express"
+import path from 'path'
 const app = express()
-
 const port = 8080
 
+// configure express to use EJS
+app.set("views", path.join(__dirname, "views"))
+app.set("view engine", "ejs")
+
 app.get('/', (req, res)=>{
-    res.send('hello there')
+    // render the ejs template
+    res.render("index")
 })
 
 app.listen(port, ()=>{
